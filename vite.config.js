50607f8d219dc19aa266e48b87707c8ae49e118b
Anthony Zoss <anthony@anthonys-macbook-pro.home>
2026-06-10 23:04:59 +0200

    Sync Vite JS config for GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 export default defineConfig({
+    base: "/Advanced-AI-UX-Tech-Collection/",
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
